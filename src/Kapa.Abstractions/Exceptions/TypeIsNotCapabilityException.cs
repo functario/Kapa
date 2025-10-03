642fa6ac20fdf9ebdfc65423c6ae7114a5fd9a13
Functario <166994080+functario@users.noreply.github.com>
2025-10-03 14:04:41 -0400

    Add Exceptions

---
+++ src/Kapa.Abstractions/Exceptions/TypeIsNotCapabilityException.cs
@@ -0,0 +1,22 @@
+﻿using Kapa.Abstractions.Capabilities;
+
+namespace Kapa.Abstractions.Exceptions;
+
+public sealed class TypeIsNotCapabilityException : Exception
+{
+    private static string GetMessage(Type type)
+    {
+        return $"{type.FullName} is not {nameof(ICapabilityType)}.";
+    }
+
+    public TypeIsNotCapabilityException(Type type)
+        : base(GetMessage(type)) { }
+
+    public TypeIsNotCapabilityException() { }
+
+    public TypeIsNotCapabilityException(string message)
+        : base(message) { }
+
+    public TypeIsNotCapabilityException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
