6a9cd7703bb1989e617aa180b3f11efcd747a82b
Functario <166994080+functario@users.noreply.github.com>
2025-10-06 15:52:31 -0400

    RequirementFactory

---
+++ src/Kapa.Core/Factories/RequirementFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using Kapa.Abstractions.Prototypes;
+using Kapa.Core.Prototypes;
+
+namespace Kapa.Core.Factories;
+
+public static class RequirementFactory
+{
+    public static Requirement<IHasTrait> Create<T>(this Expression<Func<T, bool>> expr)
+        where T : IHasTrait => new(p => expr.Compile()((T)p));
+}
