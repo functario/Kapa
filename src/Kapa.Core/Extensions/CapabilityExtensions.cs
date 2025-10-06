19b19b81149969036213a9bc595f31d6e0785e2e
Functario <166994080+functario@users.noreply.github.com>
2025-10-06 15:52:19 -0400

    ToNode

---
+++ src/Kapa.Core/Extensions/CapabilityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Kapa.Abstractions.Capabilities;
+using Kapa.Abstractions.Graphs;
+using Kapa.Core.Graphs;
+
+namespace Kapa.Core.Extensions;
+
+public static class CapabilityExtensions
+{
+    public static INode ToNode(this ICapability capability)
+    {
+        ArgumentNullException.ThrowIfNull(capability);
+        var mutations = capability.Relations?.Mutations ?? [];
+        var requirements = capability.Relations?.Requirements ?? [];
+        return new Node(capability.GetType(), mutations, requirements);
+    }
+}
